5dc1a727b3b9c7402a28ce1aba6f958762f6a225
Azahari Zaman <azaharizaman@gmail.com>
2026-07-28 07:45:38 +0800

    feat(country): Tasks 16-17 - Package exports and capability registration
    - Country/__init__.py: Exports CountryCapability, CountryContract, CountryNotation
    - capabilities/__init__.py: Register Country as built-in capability

--- paxman/capabilities/__init__.py
+++ paxman/capabilities/__init__.py
@@ -1,6 +1,7 @@
 """Paxman capabilities."""
 
+from paxman.capabilities.Country.capability import CountryCapability as Country
 from paxman.capabilities.Date.capability import DateCapability as Date
 from paxman.capabilities.Email.capability import EmailCapability as Email
 
-__all__ = ["Date", "Email"]
+__all__ = ["Country", "Date", "Email"]
