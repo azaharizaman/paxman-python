97f2157594ecb68ceb20b491b0ff34796333018a
Azahari Zaman <azaharizaman@gmail.com>
2026-08-01 22:25:12 +0800

    feat(phone): register Phone capability

--- paxman/capabilities/__init__.py
+++ paxman/capabilities/__init__.py
@@ -3,5 +3,6 @@
 from paxman.capabilities.Country.capability import CountryCapability as Country
 from paxman.capabilities.Date.capability import DateCapability as Date
 from paxman.capabilities.Email.capability import EmailCapability as Email
+from paxman.capabilities.Phone.capability import PhoneCapability as Phone
 
-__all__ = ["Country", "Date", "Email"]
+__all__ = ["Country", "Date", "Email", "Phone"]
